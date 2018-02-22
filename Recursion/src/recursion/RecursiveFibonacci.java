9c6aac7dd4525e4214c351914bd101b4213c5e97
rvickram <rvickram@uwo.ca>
2018-02-22 12:41:30 -0500

    final build
    fibonacci completed, all test passed.
    
    NOTE: Was unable to do timings!

--- Recursion/src/recursion/RecursiveFibonacci.java
+++ Recursion/src/recursion/RecursiveFibonacci.java
@@ -33,10 +33,16 @@ public class RecursiveFibonacci
     public long better(long n)
     {
         long result = 0;
-        // IMPLEMENT THIS RECURSIVE METHOD
-//>>>>>>>>> ADDED CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        
-
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<        
+        
+        if(n<=0)
+            result = 0;
+        else if(n==1)
+            result = 1;
+        else if(n%2 == 0)
+            result = better(n/2)*better(n/2) + 2*better(n/2)*better((n/2) - 1);
+        else
+            result = 2*better(n/2)*better(n/2) + 2*better(n/2)*better((n/2) - 1) + better((n/2) - 1)*better((n/2) - 1);
+        
         return result;
     }
 
@@ -49,13 +55,8 @@ public class RecursiveFibonacci
      * @return     The nth fibonacci number.
      */
     public long tailRecursive(long n)
-    {
-        // IMPLEMENT THIS METHOD USING A RECURSIVE HELPER FUNCTION
-        // AND RETURN AN APPROPRIATE VALUE
-//>>>>>>>>> REPLACEMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        
-
-        return 0;
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<        
+    {       
+        return helper(n,1,0);
     } 
     
      
@@ -70,10 +71,14 @@ public class RecursiveFibonacci
 
     public long helper(long n, long fi, long fi1)
     {  
+        System.out.println("helper called!");
         long result = 0;
-//>>>>>>>>> ADDED CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>           
-     
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<        
+        if(n <= 0) result = fi1;
+        else if(n == 1) result = fi;
+        else if(secondMSB(n))
+            result = helper(reduceBy2ndMSB(n), 2*fi*fi + 2*fi*fi1 + fi1*fi1, fi*fi + 2*fi*fi1);
+        else
+            result = helper(reduceBy2ndMSB(n), fi*fi + 2*fi*fi1, fi*fi + fi1*fi1);
         return result;
     }
        
@@ -88,9 +93,14 @@ public class RecursiveFibonacci
     {
         // IMPLEMENT THIS METHOD AND RETURN AN APPROPRIATE VALUE
 //>>>>>>>>> REPLACEMENT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        
- 
+        String bin = Long.toBinaryString(n); //convert to binary string
+        
+        if (bin.substring(1, 2).equals("1")){ //tests if second MSB is a 1
+            return true;
+        }
+        else 
+            return false;
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
-        return false;
     }
 
 
@@ -106,8 +116,10 @@ public class RecursiveFibonacci
     {
         long result = 1;
         // IMPLEMENT THIS METHOD
-//>>>>>>>>> ADDED CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        
-
+//>>>>>>>>> ADDED CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        String bin = Long.toBinaryString(n);
+        bin = bin.substring(0, 1) + bin.substring(2,bin.length());
+        result = Long.parseLong(bin, 2);
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         
         return result;
